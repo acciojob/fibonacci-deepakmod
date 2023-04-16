a5f48548dba37e86bc89d826c0da2353b5111722
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-16 21:35:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function fibonacci(num) {
-// your code here
+	let fib=[0,1];
+    for(let i=2;i<num;i++){
+        fib.push(fib[i-1]+fib[i-2]);
+    }
+    console.log(fib);
+    return fib[num-1];
 }
 
 module.exports = fibonacci;
